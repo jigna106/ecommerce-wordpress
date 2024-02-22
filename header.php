0c395f7e27a8a507ecc0fb9127849b20e41a2750
Jigna Raval <jraval106001@gmail.com>
2024-02-23 00:01:21 +0530

    fix css

--- header.php
+++ header.php
@@ -1,22 +1,24 @@
 <html <?php language_attributes(); ?>>
 
 <head class="menu-header">
-    <title><?php wp_title(); ?></title>
+  <title>
+    <?php wp_title(); ?>
+  </title>
 
-    <?php session_start();
-    wp_head(); ?>
+  <?php session_start();
+  wp_head(); ?>
 </head>
 
 <body <?php body_class(); ?>>
 
 
-<nav class="navbar navbar-expand-lg bg-body-tertiary" id="navigation">
-  <div class="container-fluid">
-    <div class="header-logo">
+  <nav class="navbar navbar-expand-lg bg-body-tertiary" id="navigation">
+    <div class="container-fluid">
+      <div class="header-logo">
         <img src="<?php echo get_theme_file_uri('assets/images/logo1.png'); ?>" />
-    </div>
-    <div class="header-link">
+      </div>
+      <div class="header-link">
         <?php wp_nav_menu(array('menu' => 'header-menu')); ?>
+      </div>
     </div>
-  </div>
-</nav>
+  </nav>
--- template-cart.php
+++ template-cart.php
@@ -15,90 +15,100 @@ if (isset($_POST['qtysubmit'])) {
 }
 
 if (isset($_SESSION['productitems'])) {
-?>
-    <div class="empty-cart">
-        <form method="post">
-            <button type="submit" name="emptycartsubmit" class="btn btn-primary"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="red" class="bi bi-trash-fill" viewBox="0 0 16 16">
-                    <path d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5M8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5m3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0" />
-                </svg> Empty Cart</button>
-            <!-- <input type="submit" name=emptycartsubmit value="Empty Cart" /> -->
-        </form>
-    </div>
-    <?php
     $grandtotal = 100;
     ?>
-    <h1>Bill Summary</h1>
-    <div class="container">
-    <div class="shopping-cart">
-    
-    <div class="column-labels">
-            <label class="product-image">Image</label>
-            <label class="product-details">Product</label>
-            <label class="product-price">Price</label>
-            <label class="product-quantity">Quantity</label>
-            <label class="product-removal">Remove</label>
-            <label class="product-line-price">Total</label>
+    <div class="container mt-5 mb-5">
+        <div class="row pt-3">
+            <form method="post" class="d-flex justify-content-end align-items-center">
+                <button type="submit" name="emptycartsubmit" class="btn btn-danger"><svg xmlns="http://www.w3.org/2000/svg"
+                        width="16" height="16" fill="white" class="bi bi-trash-fill" viewBox="0 0 16 16">
+                        <path
+                            d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5M8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5m3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0" />
+                    </svg> Empty Cart</button>
+                <!-- <input type="submit" name=emptycartsubmit value="Empty Cart" /> -->
+            </form>
         </div>
-    
-       
-        <?php
-        foreach ($_SESSION['productitems'] as $product_id => $qty) {
-            $product = get_post($product_id);
-        ?>
-    
-            <div class="product">
-                <div class="product-image">
-                    <img src="<?php echo  get_the_post_thumbnail_url($product_id) ?>" width="100px" height="100px">
+        <div class="row">
+            <h1 class="d-flex justify-content-center align-items-center">Cart Details</h1>
+        </div>
+        <div class="row product-section">
+            <div class="col-10 product-table">
+                <div class="row">
+                    <div class="col-2 pt-3"><b>Image</b></div>
+                    <div class="col-2 pt-3"><b>Product</b></div>
+                    <div class="col-2 pt-3"><b>Price</b></div>
+                    <div class="col-2 pt-3"><b>Quantity</b></div>
+                    <div class="col-2 pt-3"><b>Sub Total</b></div>
+                    <div class="col-2 pt-3"><b>Remove Product</b></div>
                 </div>
-                <div class="product-details">
-                    <div class="product-title"> <?php echo get_the_title($product_id); ?> </div>
-                </div>
-                <div class="product-price"><?php echo get_post_meta($product_id, "ecommerce_price", true); ?></div>
-                <div class="product-quantity">
+                <?php
+                foreach ($_SESSION['productitems'] as $product_id => $qty) {
+                    $product = get_post($product_id);
+                    ?>
 
-                    <form method="post">
-                        Quantity :
-                        <input type="hidden" name="hiddenid" value="<?php echo $product_id ?>" />
-                        <input type="number" id="quantity" name="quantity" min="1" value="<?php echo $qty; ?>" />
-                        <input type="submit" name="qtysubmit" value="update quantity" />
-                    </form>
+                    <div class="row">
+                        <div class="col-2 pt-3">
+                            <img src="<?php echo get_the_post_thumbnail_url($product_id) ?>" width="100px" height="100px">
+                        </div>
+                        <div class="col-2 pt-3">
+                            <?php echo get_the_title($product_id); ?>
+                        </div>
+                        <div class="col-2 pt-3">
+                            <?php echo get_post_meta($product_id, "ecommerce_price", true); ?>
+                        </div>
+                        <div class="col-2 pt-3">
+                            <form method="post">
+                                <input type="hidden" name="hiddenid" value="<?php echo $product_id ?>" />
+                                <input type="number" class="w-75" id="quantity" name="quantity" min="1"
+                                    value="<?php echo $qty; ?>" />
+                                <input type="submit" name="qtysubmit" value="Update Quantity" />
+                            </form>
+                        </div>
+                        <div class="col-2 pt-3">
+                            <?php
+                            $subtotal = get_post_meta($product_id, "ecommerce_price", true) * (int) $qty; ?>
+                            <?php echo $subtotal;
+                            $grandtotal += $subtotal; ?>
+
+                        </div>
+                        <div class="col-2 pt-3">
+                            <button class="btn btn-danger">
+                                Remove
+                            </button>
+                        </div>
+                    </div>
 
 
-                </div>
-                <div class="product-removal">
-                    <button class="remove-product">
-                        Remove
-                    </button>
-                </div>
-                <div class="product-line-price">
-
-                    <p> <?php
-                        $subtotal = get_post_meta($product_id, "ecommerce_price", true) * (int)$qty; ?>
-                        <?php echo  $subtotal;
-                        $grandtotal += $subtotal ?></p>
-                </div>
+                    <?php
+                }
+                ?>
             </div>
+            <div class="col-2 billing-details">
+                <div class="totals">
+                    <div class="totals-item">
+                        <label>Shipping</label>
+                        <div class="totals-value" id="cart-shipping">100</div>
+                    </div>
+                    <div class="totals-item totals-item-total">
+                        <label>Grand Total</label>
+                        <div class="totals-value" id="cart-total">
+                            <?php //echo $grandtotal;                 ?>
+                        </div>
+                    </div>
+                </div>
 
 
-        <?php
-        }
-        ?>
-        <div class="totals">
-            <div class="totals-item">
-                <label>Shipping</label>
-                <div class="totals-value" id="cart-shipping">100</div>
-            </div>
-            <div class="totals-item totals-item-total">
-                <label>Grand Total</label>
-                <div class="totals-value" id="cart-total"><?php echo $grandtotal; ?></div>
+                <button class="checkout">Checkout</button>
             </div>
         </div>
+    </div>
 
 
-        <button class="checkout">Checkout</button>
+
+
     </div>
-    </div>
-<?php
+
+    <?php
 } else {
     echo "Your cart empty";
 }
